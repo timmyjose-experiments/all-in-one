e1600b680f6d5aea40c0c81f1a91e38ea7dc0380
Timmy Jose <zoltan.jose@gmail.com>
2024-11-12 13:58:25 +0530

    feat: add expo-location, ipdate, and ipify demos; add workflow for eas updates

---
+++ src/screens/LocationDemo.tsx
@@ -0,0 +1,47 @@
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+import { Pressable, View, Text } from 'react-native'
+import styles from '../styles'
+import { useEffect, useState } from 'react'
+import * as Location from 'expo-location'
+
+const LocationDemo = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+  const [errorMsg, setErrorMsg] = useState<string | null>(null)
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied')
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync()
+      setLocation(location)
+    })()
+  }, [])
+
+  let text = 'Waiting...'
+  if (errorMsg) {
+    text = errorMsg
+  } else if (location) {
+    text = JSON.stringify(location, null, 2)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{text}</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate('Home')}>
+        <Text>Home</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default LocationDemo
--- src/screens/NetworkInfoDemo.tsx
+++ src/screens/NetworkInfoDemo.tsx
@@ -3,12 +3,19 @@ import styles from '../styles'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../App'
+import usePublicIp from '../features/usePublicIp'
 
 const NetworkInfoDemo = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
+  const { ipifyIpAddress, ipifyError, ipdataIpAddress, ipdataError } = usePublicIp()
+
   return (
     <View style={styles.container}>
+      { !!ipifyError && (<Text>ipify: {ipifyError}</Text>)}
+      { !!ipdataError && (<Text>ipdata: {ipdataError}</Text>)}
+      <Text>IP Address (ipify): {ipifyIpAddress ?? 'n/a'}</Text>
+      <Text>IP Address (ipdata): {ipdataIpAddress ?? 'n/a'}</Text>
       <Pressable
         style={styles.button}
         onPress={() => navigation.navigate('Home')}>
